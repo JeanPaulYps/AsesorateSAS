00782c59e1b8a965425a3d890806369a83d255bc
Jean Paul <yjeanpaul@gmail.com>
2019-06-26 17:24:26 -0500

    se configuro conexion a Base de datos PostgreSQL en el servidor

--- AnacondaSolutions/settings.py
+++ AnacondaSolutions/settings.py
@@ -90,12 +90,12 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'asesorate_sas',
-        'USER': 'asesorate',
+        'USER': 'asesorate@asesoratesas',
         'PASSWORD': 't^g\d4HEX',
-        'HOST':'asesoratesas.postgres.database.azure.com',
-        'PORT':'5432',
+        'HOST': 'asesoratesas.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 

bf6113a67440432df85544cbc6da397831209d3c
Luis Carlos <lcarrietas@unal.edu.co>
2019-06-26 20:22:40 -0500

    Agregado precio al modelo cotización y método que calcula el precio.

--- AnacondaSolutions/asesorate_sas/models.py
+++ AnacondaSolutions/asesorate_sas/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+#necesario instalar computed_property
+from computed_property import ComputedTextField
 
 # Create your models here.
 
+#MODELO COTIZACIÓN
 #MODELO COTIZACIÓN
 class Cotizacion(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,9 +13,10 @@ class Cotizacion(models.Model):
     id_categoria = models.TextField(null=False,
     choices=([('JR','JUNIOR'),('SB','SENIOR BASIC'),('SM','SENIOR MEDIUM'),
     ('SA1','SENIOR ADVANCED 1'),('SA2','SENIOR ADVANCED 2')]) )
-
-    #Para consultar el precio, simplemente se llama el metodo get_precio()
-    def get_precio(self):
+    precio = ComputedTextField(compute_from='set_precio')
+    
+    @property
+    def set_precio(self):
         VALOR_HORA = {
             'JR':33000,
             'SB':44000,
@@ -21,6 +25,10 @@ class Cotizacion(models.Model):
             'SA2':60000
         }
         return self.horas*VALOR_HORA[self.id_categoria]
+      
+#Para consultar el precio, simplemente se llama el metodo get_precio()    
+    def get_precio(self):
+        return  self.precio
         
 class Estudiante(models.Model):
     nombre = models.CharField(max_length = 150)

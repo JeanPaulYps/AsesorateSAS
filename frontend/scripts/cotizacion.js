cc7b45f82dea1bd3215b1d3215c770563c39bd83
Carlos Bz <cdbzprogr@gmail.com>
2019-07-03 21:46:17 -0500

    Cotizacion funcional.

--- frontend/scripts/cotizacion.js
+++ frontend/scripts/cotizacion.js
@@ -1,38 +1,35 @@
 $("form").submit(function(event) {
-            event.preventDefault();
-            var horas = jQuery("[name=horas]").val();
-            var subject = jQuery("[name=subject]").val();
-            var precio = 0;
-            var identificador = localStorage.getItem("cedula");
-
-
-
-            fetch('https://fathomless-mesa-60059.herokuapp.com/api/registroCotizacion', {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json, text-plain, */*"
-                },
-                method: 'post',
-                credentials: "same-origin",
-                body: JSON.stringify({
-                    id_estudiante: identificador,
-                    horas: horas,
-                    categoria: subject,
-					precio:precio
-                    
-                })
-            })
-                .then((data) => data.json())
-                .then(res =>{
-                    
-					document.getElementById("mensaje").innerHTML=res.precio;
-                   
-                })
-                .catch(function(error) {
-                  
-                    console.log(error);
-                });
+    event.preventDefault();
+    var horas = jQuery("[name=horas]").val();
+    var subject = jQuery("[name=subject]").val();
+    var precio = 0;
+    var identificador = localStorage.getItem("cedula");
+    
+    fetch('https://fathomless-mesa-60059.herokuapp.com/api/registroCotizacion', {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*"
+        },
+        method: 'post',
+        credentials: "same-origin",
+        body: JSON.stringify({
+            id_estudiante: identificador,
+            horas: horas,
+            nivel: subject,
+			precio:precio
+            
+        })
+    })
+        .then((data) => data.json())
+        .then(res =>{
+            document.getElementById("cotizacion").style.visibility = "visible";
+			document.getElementById("mensaje").innerHTML=res.precio;
+           
+        })
+        .catch(function(error) {
+            console.log(error);
         });
+});
 
 
 

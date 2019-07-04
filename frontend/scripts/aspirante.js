ba0ce2f4890a7eefd1c9f749905041f45bb5a9e5
Carlos Bz <cdbzprogr@gmail.com>
2019-07-04 12:38:16 -0500

    Verificacion de correo y cedula

--- frontend/scripts/aspirante.js
+++ frontend/scripts/aspirante.js
@@ -44,7 +44,7 @@ function validarCorreo(){
     var btnConfirmar= document.querySelector("#confirmar")
     btnConfirmar.disabled = true
 
-    fetch('https://fathomless-mesa-60059.herokuapp.com/api/verificarCorreoAspirante', {
+    fetch('https://fathomless-mesa-60059.herokuapp.com/api/verificarCorreo', {
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json, text-plain, */*"
@@ -52,7 +52,45 @@ function validarCorreo(){
         method: 'post',
         credentials: "same-origin",
         body: JSON.stringify({
-            correo: email
+            correo: email,
+            modelo: 'Aspirante'
+        })
+    })
+        .then((data) => data.json())
+        .then(res =>{
+            if (res.message){
+                validador.css('visibility','hidden')
+                validador.css('position','absolute')
+                btnConfirmar.disabled = false
+                return true;
+            }else{
+                validador.css('position','relative')
+                validador.css('visibility','visible')
+                btnConfirmar.disabled = true
+                return false;
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+function validarCedula(){
+    var cedula = jQuery("[name=cedula]").val();
+    var validador = jQuery("[name=ccvalidador]");
+    var btnConfirmar= document.querySelector("#confirmar")
+    btnConfirmar.disabled = true
+
+    fetch('https://fathomless-mesa-60059.herokuapp.com/api/verificarCedula', {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*"
+        },
+        method: 'post',
+        credentials: "same-origin",
+        body: JSON.stringify({
+            cedula: cedula,
+            modelo: 'Aspirante'
         })
     })
         .then((data) => data.json())
--- frontend/scripts/gestionTutor.js
+++ frontend/scripts/gestionTutor.js
@@ -216,3 +216,77 @@ function indexInClass(node,myClass) {
     }
     return -1;
 }
+
+function validarCorreo(){
+    var email = jQuery("[name=email]").val();
+    var validador = jQuery("[name=cvalidador]");
+    var btnConfirmar= document.querySelector("#confirmar")
+    btnConfirmar.disabled = true
+
+    fetch('https://fathomless-mesa-60059.herokuapp.com/api/verificarCorreo', {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*"
+        },
+        method: 'post',
+        credentials: "same-origin",
+        body: JSON.stringify({
+            correo: email,
+            modelo: 'Tutor'
+        })
+    })
+        .then((data) => data.json())
+        .then(res =>{
+            if (res.message){
+                validador.css('visibility','hidden')
+                validador.css('position','absolute')
+                btnConfirmar.disabled = false
+                return true;
+            }else{
+                validador.css('position','relative')
+                validador.css('visibility','visible')
+                btnConfirmar.disabled = true
+                return false;
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+function validarCedula(){
+    var cedula = jQuery("[name=cedula]").val();
+    var validador = jQuery("[name=ccvalidador]");
+    var btnConfirmar= document.querySelector("#confirmar")
+    btnConfirmar.disabled = true
+
+    fetch('https://fathomless-mesa-60059.herokuapp.com/api/verificarCedula', {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*"
+        },
+        method: 'post',
+        credentials: "same-origin",
+        body: JSON.stringify({
+            cedula: cedula,
+            modelo: 'Tutor'
+        })
+    })
+        .then((data) => data.json())
+        .then(res =>{
+            if (res.message){
+                validador.css('visibility','hidden')
+                validador.css('position','absolute')
+                btnConfirmar.disabled = false
+                return true;
+            }else{
+                validador.css('position','relative')
+                validador.css('visibility','visible')
+                btnConfirmar.disabled = true
+                return false;
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}

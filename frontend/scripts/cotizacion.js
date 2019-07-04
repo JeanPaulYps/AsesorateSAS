efa8401352b0711ec80375cf5c31cd4595614374
felinblackcat <47456740+felinblackcat@users.noreply.github.com>
2019-07-03 21:35:33 -0500

    Agregando js cotizacion

---
+++ frontend/scripts/cotizacion.js
@@ -0,0 +1,39 @@
+$("form").submit(function(event) {
+            event.preventDefault();
+            var horas = jQuery("[name=horas]").val();
+            var subject = jQuery("[name=subject]").val();
+            var precio = 0;
+            var identificador = localStorage.getItem("cedula");
+
+
+
+            fetch('https://fathomless-mesa-60059.herokuapp.com/api/registroCotizacion', {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json, text-plain, */*"
+                },
+                method: 'post',
+                credentials: "same-origin",
+                body: JSON.stringify({
+                    id_estudiante: identificador,
+                    horas: horas,
+                    categoria: subject,
+					precio:precio
+                    
+                })
+            })
+                .then((data) => data.json())
+                .then(res =>{
+                    
+					document.getElementById("mensaje").innerHTML=res.precio;
+                   
+                })
+                .catch(function(error) {
+                  
+                    console.log(error);
+                });
+        });
+
+
+
+

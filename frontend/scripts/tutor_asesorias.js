eb367038ef3fbb421f177d9b735c438610bfe787
KewinEvers96 <keversy@unal.edu.co>
2019-08-01 03:16:05 -0500

    listar reservas para tutor

--- frontend/scripts/tutor_asesorias.js
+++ frontend/scripts/tutor_asesorias.js
@@ -1,1 +1,80 @@
-var reservas = []
+var reservas = [];
+var cedula_tutor = localStorage.getItem('cedula');
+
+window.onload = async () =>{
+    renovarTabla();
+}
+
+async function listarReservas() {
+    await fetch("https://fathomless-mesa-60059.herokuapp.com/api/listarReservas",{
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json, text-plain, */*"
+        },
+        method:'post',
+        credentials:"same-origin",
+        body:JSON.stringify({
+            cedula_tutor:cedula_tutor
+        })
+    })
+        .then((data) => data.json())
+        .then(res => {
+            console.log(res.reservas);
+            reservas = res.reservas;
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
+
+async function dibujarTabla(){
+    dom = ``;
+    reservas.map(
+        (reserva) =>{
+            dom = dom + `
+            <tr>
+                <td>${reserva.nombre}</td>
+                <td>${reserva.correo}</td>
+                <td>29-02-2020</td>
+                <td>hora inicio</td>
+                <td>hora fin </td>
+                <td>
+                <div class="col-sm-6">
+                    <a href= "" data-id=${reserva.cedula_estudiante} class="btn btn-info parano" data-toggle="modal">Realizada</a>
+                </div>
+                </td>
+            </tr>
+            `
+        }
+    )
+}
+
+$(document).on("click", ".parano", function(){
+    var cedula_estudiante = $this.attr('data-id');
+    realizarAsesoria(cedula_estudiante);
+})
+
+async function renovarTabla(){
+    await listarReservas();
+    dibujarTabla();
+}
+
+async function realizarAsesoria(cedula_estudiante){
+    await fetch("https://fathomless-mesa-60059.herokuapp.com/api/realizarReserva",{
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json, text-plain, */*"
+        },
+        method:'post',
+        credentials:"same-origin",
+        body:JSON.stringify({
+            cedula_estudiante:cedula_estudiante,
+            cedula_tutor : cedula_tutor
+        })
+    })
+        .then((data) => data.json())
+        .then(res => {
+            console.log(res.message);
+        }).catch(function(error) {
+            console.log(error);
+        });
+}
